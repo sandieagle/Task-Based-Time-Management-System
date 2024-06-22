0cf8d8dff8200a1adf7b7a4729f120842f6ecc3e
sandieagle <jayasundarasanduni49@gmail.com>
2024-06-22 18:53:57 +0530

    Files Upload

--- Project full stack/employeems.sql
+++ Project full stack/employeems.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 20, 2024 at 07:42 PM
+-- Generation Time: Apr 23, 2024 at 02:26 PM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,7 +38,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `email`, `password`) VALUES
-(3, 'shehandjayasundara@gmail.com', 'Shehanddr');
+(4, 'jayasundarasanduni49@gmail.com', '19990507sandi');
 
 -- --------------------------------------------------------
 
@@ -61,7 +61,8 @@ INSERT INTO `category` (`id`, `name`) VALUES
 (3, 'Staff'),
 (4, 'Science'),
 (5, 'Art'),
-(6, 'Cleaning');
+(6, 'Cleaning'),
+(7, 'HR');
 
 -- --------------------------------------------------------
 
@@ -85,9 +86,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`id`, `name`, `email`, `password`, `salary`, `address`, `image`, `category_id`) VALUES
-(16, 'Nuwan', 'yasundara@gmail.com', '$2b$10$o1/i0i5UTkDPgkBdSf5gLOsZmtfRd4CVvCo8wBGr.MOAjdLmw9OT.', 600, 'Mirigama', 'image_1713529899518.jpg', 3),
-(17, 'show1', 'shehandjayasundara@gmail.com', '$2b$10$Mr/84LlNiLtwd0/rEwSkWuhuTeD47OrQ9UoFyccCceq8b5u86msIm', 400, 'Colombo', 'image_1713530963802.jpg', 2),
-(19, 'Shanaka', 'Shanakajayasundara@gmail.com', '$2b$10$35dhMXCW6u8VAtXa.1rkRunOBwh.Ay8EZOcM0jCGZ.1n.kb7BLEHe', 500, 'colombo', 'image_1713551372997.jpg', 2);
+(31, 'Nirmani', 'jayadewanir99@gmail.com', '$2b$10$ZaCyJAJI0vxlcJ1OgQ4cTemmU061gnTvHh6c.KPNo981Oqm5HeQ8.', 250, 'Kegalle', 'image_1713870714163.jpg', 2),
+(32, 'Dinitha', 'bandaradini59@gmail.com', '$2b$10$vSvoabmndfew/oMgx0eCbOY1sFHPBCYAePUeKfxhmdlyDa2qbubVa', 785, 'Gampaha', 'image_1713870967580.jpg', 2),
+(33, 'Thashmini', 'thasmisomaweera@gmail.com', '$2b$10$mUw3bAAKV9SnFy2OWv7u9OK25Rlfk4Ft5LkqwfFU7/CPRkeVeKp2u', 670, 'Kegalle', 'image_1713871144001.jpg', 4);
 
 -- --------------------------------------------------------
 
@@ -112,14 +113,11 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `title`, `description`, `priority`, `dueDate`, `completed`, `created_at`, `updated_at`, `completed_at`) VALUES
-(1, 'Task 1', 'Description for Task 1', 'High', '2024-01-05', 0, '2024-04-19 08:41:29', '2024-04-19 08:41:29', NULL),
-(2, 'Task 2', 'Description for Task 2', 'Medium', '2024-01-06', 0, '2024-04-19 08:41:29', '2024-04-19 08:41:29', NULL),
-(3, 'Task 3', 'Description for Task 3', 'Low', '2024-01-07', 0, '2024-04-19 08:41:29', '2024-04-19 08:41:29', NULL),
-(4, 'Task 4', 'Description for Task 4', 'High', '2024-01-08', 0, '2024-04-19 08:41:29', '2024-04-19 08:41:29', NULL),
-(5, 'Task 5', 'Description for Task 5', 'Medium', '2024-01-09', 0, '2024-04-19 08:41:29', '2024-04-19 08:41:29', NULL),
-(6, 'Task 6', 'Description for Task 6', 'Low', '2024-01-10', 0, '2024-04-19 08:41:29', '2024-04-19 08:41:29', NULL),
-(7, 'Task 7', 'Description for Task 7', 'High', '2024-01-11', 0, '2024-04-19 08:41:29', '2024-04-19 08:41:29', NULL),
-(8, 'Task 8', 'Description for Task 8', 'Medium', '2024-01-12', 0, '2024-04-19 08:41:29', '2024-04-19 08:41:29', NULL);
+(1, 'Budget management', 'Develop department monthly budget and perform them in charts.', 'Medium', '2024-04-25', 0, '2024-04-19 08:41:29', '2024-04-23 11:28:30', NULL),
+(2, 'Staff training', 'provide training of orientation sessions for volunteers.', 'Low', '2024-04-26', 0, '2024-04-19 08:41:29', '2024-04-23 11:30:25', NULL),
+(3, 'Department workshop', 'organize workshops to engage attendees with scientific concepts.', 'High', '2024-04-30', 0, '2024-04-19 08:41:29', '2024-04-23 11:33:49', NULL),
+(21, 'Cleaning management', 'arrange for cleaning services before, during, and after the event \"Nethra\"', 'Medium', '2024-04-22', 1, '2024-04-20 18:02:54', '2024-04-23 11:35:35', NULL),
+(22, 'Celebrations', 'department 21st anniversary celebration', 'High', '2024-05-02', 0, '2024-04-21 04:58:57', '2024-04-23 11:38:11', NULL);
 
 --
 -- Indexes for dumped tables
@@ -158,25 +156,25 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
